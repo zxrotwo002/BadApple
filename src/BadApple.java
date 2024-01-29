be00ebd1200f9927f096f4fa61a35d6af671026b
zxrotwo002 <habromleo@gmail.com>
2024-01-29 13:23:45 +0100

    Bad Apple

--- src/BadApple.java
+++ src/BadApple.java
@@ -1,85 +1,37 @@
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.util.StringTokenizer;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.applet.Applet;
-import java.applet.AudioClip;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 class BadApple extends JFrame {
-    private static AudioClip music;
-    private static javax.swing.Timer timer;
-    private Listener listener;
+    private static Clip music;
 
     public BadApple() {
-        music = Applet
-                .newAudioClip(this.getClass().getResource("BadApple.wav"));
-        listener = new Listener();
-        getContentPane().add(listener);
-        timer = new javax.swing.Timer(32, listener);
-
+        try {
+            AudioInputStream stream = AudioSystem
+                    .getAudioInputStream(Objects.requireNonNull(this.getClass()
+                            .getResource("BadApple.wav")));
+            music = AudioSystem.getClip();
+            music.open(stream);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        VideoPanel videoPanel = new VideoPanel();
+        add(videoPanel);
         setTitle("BadApple");
+        setBackground(Color.black);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(650, 510);
+        setResizable(false);
         setVisible(true);
+        videoPanel.startVideoThread();
     }
 
     public static void main(String[] args) {
         new BadApple();
-        music.play();
-        timer.start();
+        music.start();
     }
 
-    class Listener extends JPanel implements ActionListener {
-        private String fileName;
-        private int i = 1;
-
-        public void actionPerformed(ActionEvent e) {
-            repaint();
-        }
-
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            if (i == 6569) {
-                System.exit(0);
-            }
-            else {
-                i++;
-            }
-
-            fileName = "data/out (" + i + ").txt";
-            FileReader fr = null;
-            BufferedReader br = null;
-            try {
-                fr = new FileReader(fileName);
-                br = new BufferedReader(fr);
-                String temp;
-
-                int j = 0;
-
-                while ((temp = br.readLine()) != null) {
-                    StringTokenizer st = new StringTokenizer(temp);
-                    while (st.hasMoreTokens()) {
-                        int start = Integer.parseInt(st.nextToken());
-                        int end = Integer.parseInt(st.nextToken());
-                        g.drawLine(start, j, end, j);
-                    }
-                    j++;
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            } finally {
-                try {
-                    fr.close();
-                    br.close();
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
-        }
-    }
 }
--- src/VideoPanel.java
+++ src/VideoPanel.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class VideoPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class VideoPanel extends JPanel implements Runnable {
+    private int i = 1;
+    private Thread videoThread;
+
+    public void startVideoThread() {
+        videoThread = new Thread(this);
+        videoThread.start();
+    }
+
+    public void run() {
+        double drawInterval = 1000000000.0 / 30;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+
+        while (videoThread != null) {
+            currentTime = System.nanoTime();
+            delta = delta + (currentTime - lastTime) / drawInterval;
+            timer = timer + (currentTime - lastTime);
+            lastTime = currentTime;
+            if (delta >= 1) {
+                repaint();
+                delta = delta - 1;
+                drawCount = drawCount + 1;
+            }
+
+            if (timer >= 1000000000.0) {
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.getColor(null,4587570));
+        g2.fillRect(0,0,650,510);
+        if (i == 6569) {
+            System.exit(0);
+        }
+        else {
+            i++;
+        }
+
+        String fileName = "/data/out (" + i + ").txt";
+        InputStream is = null;
+        BufferedReader br = null;
+        try {
+            is = getClass().getResourceAsStream(fileName);
+            assert is != null;
+            br = new BufferedReader(new InputStreamReader(is));
+            String temp;
+
+            int j = 0;
+
+            while ((temp = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(temp);
+                while (st.hasMoreTokens()) {
+                    int start = Integer.parseInt(st.nextToken());
+                    int end = Integer.parseInt(st.nextToken());
+                    g2.setColor(Color.black);
+                    g2.fillRect(start, j, end - start, 1);
+                }
+                j++;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                assert is != null;
+                is.close();
+                assert br != null;
+                br.close();
+            } catch (Exception ignored) {
+            }
+        }
+    }
 }
